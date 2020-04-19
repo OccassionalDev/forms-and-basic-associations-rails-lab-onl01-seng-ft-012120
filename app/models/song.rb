89b64829a51e40c05477ef36391c72e1b418a110
Erica Schimmel <ericaschimmel01@gmail.com>
2020-04-19 20:07:45 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -4,5 +4,11 @@ class Song < ActiveRecord::Base
   belongs_to :genre 
   has_many :notes
   
-  
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+
+  def artist_name
+    artist.try(:name)
+  end
 end
